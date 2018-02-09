caeb438ff815bda6eeda10a917abeadcf66749cb
Craig Dawson <craig.dawson90@gmail.com>
2018-02-09 17:48:18 +0000

    Public/ Shared Paths implemented

---
+++ Public/Login/login.php
@@ -0,0 +1,19 @@
+<section class="loginblock">
+  <div class="row justify-content-center">
+    <div class="col-md-6">
+    <form class="loginform" action="index.php">
+      <h3><?php echo $page_title ?></h3>
+      <hr>
+  <div class="form-group">
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email Address">
+  </div>
+  <div class="form-group">
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <hr>
+  <button type="submit" class="btn btn-primary">Login</button>
+
+</form>
+</div>
+  </div>
+</section>
--- Public/index.php
+++ Public/index.php
@@ -1,22 +1,5 @@
 <?php require_once('../Private/initialise.php');?>
 <?php $page_title = "Login Page"; ?>
-<?php include('../private/shared/header.php');?>
-<section class="loginblock">
-  <div class="row justify-content-center">
-    <div class="col-md-6">
-    <form class="loginform">
-      <h3><?php echo $page_title ?></h3>
-      <hr>
-  <div class="form-group">
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email Address">
-  </div>
-  <div class="form-group">
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <hr>
-  <button type="submit" class="btn btn-primary">Login</button>
-</form>
-</div>
-  </div>
-</section>
-<?php include('../private/shared/footer.php');?>
+<?php include(SHARED_PATH . '/header.php');?>
+<?php include(PUBLIC_PATH . '/login/login.php');?>
+<?php include(SHARED_PATH . '/footer.php');?>
