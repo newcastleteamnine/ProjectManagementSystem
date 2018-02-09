aed36e69f6cc64e6d93782b17336d4c8d0caf87c
Craig Dawson <craig.dawson90@gmail.com>
2018-02-09 18:27:51 +0000

    Login Path Changes

--- Private/initialise.php
+++ Private/initialise.php
@@ -5,6 +5,7 @@ define("PROJECT_PATH", dirname(PRIVATE_PATH));
 define("PUBLIC_PATH", PROJECT_PATH . '/Public');
 define("SHARED_PATH", PRIVATE_PATH . '/Shared');
 
+
 $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
 $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
 define("WWW_ROOT", $doc_root);
